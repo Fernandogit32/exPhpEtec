889125afbaf86a86ca09c0805eb30e64ebf5aca3
fernando12 <fernandogit32>
2018-02-27 21:10:05 -0300

    27/02/2018

---
+++ lista2/exercicio1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>exercicio1</title>
+</head>
+<body>
+<?php
+$n1=5;
+$n2=10;
+
+for ($i=$n1;$n1<=$n2;$n1++){
+    echo"<ul><li>".$n1."</li></ul>";
+}
+
+?>
+
+    
+
+</ul>
+</body>
+</html>
---
+++ lista2/exercicio2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$n1=10;
+
+for($n1;$n1>=0;$n1--){
+    echo"<br />$n1";
+}
+?>
+    
+</body>
+</html>
